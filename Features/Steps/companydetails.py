8a424ec421d6d9dd11920ebcc92df9cff2758643
sudhanshupandey123 <sudhanshupandey848@gmail.com>
2023-07-28 15:17:04 +0530

    first commit

--- Features/Steps/companydetails.py
+++ Features/Steps/companydetails.py
@@ -8,87 +8,58 @@ from selenium.webdriver.common.keys import Keys
 import time
 import csv
 import re
-Details=[]
+
+Details = []
+
+
 @given(u'He Opened Google Page')
 def opening_google(context):
-    context.driver=webdriver.Chrome()
+    context.driver = webdriver.Chrome()
     context.driver.get('https://www.google.com/')
     context.driver.maximize_window()
 
 
-
 @when(u'He Search Company Name "{company_name}"')
-def step_impl(context,company_name):
-    context.company_name=company_name
-    context.driver.find_element(By.XPATH,"//*[@type='search']").send_keys(company_name,Keys.ENTER)
+def searching_company(context, company_name):
+    context.company_name = company_name
+    context.driver.find_element(By.XPATH, "//*[@type='search']").send_keys(company_name, Keys.ENTER)
 
 
-@when(u'He Look For Card Apperance')
-def step_impl(context):
+@when(u'He Save Details Of Company')
+def extracting_information(context):
+    D = {}
     try:
-        context.driver.find_element(By.XPATH,"//*[@id='_oKS_ZN3BA4_cseMP7YepoAI_42']")
+        D['name'] = context.driver.find_element(By.XPATH, "(//*[@class='SPZz6b']/descendant::span)[1]").text
     except:
-        context.name=context.company_name
-        context.address='Null'
-        context.rating='Null'
-        context.reviews='Null'
-        context.Log_and_Lat='Null'
-        D = {}
-        D['Company_Name'] = context.name
-        D['Company_Address'] = context.address
-        D['Google_Rating'] = context.rating
-        D['Reviews'] = context.reviews
-        D['Log_and_Lat'] = context.Log_and_Lat.replace('@', '')
-        Details.append(D)
-        field_names = ['Company_Name', 'Company_Address', 'Google_Rating', 'Reviews', 'Log_and_Lat']
-        with open('Company_Details.csv', 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=field_names)
-            writer.writeheader()
-            writer.writerows(Details)
-
-
-
-@then(u'He Save Details Of Company')
-def step_impl(context):
+        D['name'] = 'Null'
     try:
-        context.name = context.driver.find_element(By.XPATH, "(//*[@class='SPZz6b']/descendant::span)[1]").text
-        context.address = context.driver.find_element(By.XPATH, "(//span[@class='LrzXr'])").text
-        context.rating = context.driver.find_element(By.XPATH, "(//*[@class='CJQ04']/descendant::span)[1]").text
-        context.reviews = context.driver.find_element(By.XPATH, "(//*[@class='CJQ04']/descendant::a)").text
+        D['address'] = context.driver.find_element(By.XPATH, "(//span[@class='LrzXr'])").text
+    except:
+        D['address'] = 'Null'
+    try:
+        D['rating'] = context.driver.find_element(By.XPATH, "(//*[@class='CJQ04']/descendant::span)[1]").text
+    except:
+        D['rating'] = 'Null'
+    try:
+        D['reviews'] = context.driver.find_element(By.XPATH, "(//*[@class='CJQ04']/descendant::a)").text
+    except:
+        D['reviews'] = 'Null'
+    try:
         directions = context.driver.find_element(By.XPATH, "(//div[@class='QqG1Sd'])[2]").click()
         time.sleep(5)
         url = str(context.driver.current_url)
         lan = re.search('@\S+,\S+,', url)
-        context.Log_and_Lat = lan.group()
-        D = {}
-        D['Company_Name'] = context.name
-        D['Company_Address'] = context.address
-        D['Google_Rating'] = context.rating
-        D['Reviews'] = context.reviews
-        D['Log_and_Lat'] = context.Log_and_Lat.replace('@', '')
-        Details.append(D)
-        field_names = ['Company_Name', 'Company_Address', 'Google_Rating', 'Reviews', 'Log_and_Lat']
-        with open('Company_Details.csv', 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=field_names)
-            writer.writeheader()
-            writer.writerows(Details)
-    except Exception as msg:
-        print(msg)
+        Log_and_Lat = lan.group()
+        D['Log_and_Lat'] = Log_and_Lat.replace('@', '')
+    except:
+        D['Log_and_Lat'] = 'Null'
+    Details.append(D)
 
 
-#s='https://www.google.com/maps/dir/12.9614549,77.7108679/g7cr/@12.977352,77.5817119,12z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bae167e2fa71621:0x1f56c87698101c58!2m2!1d77.5989907!2d12.9760336?entry=ttu'
-# import re
-# res=re.search('@\S+/',s)
-# print(res.group())
-
-# driver=webdriver.Chrome()
-# driver=webdriver.Chrome()
-# driver.get('https://www.google.com/')
-# driver.maximize_window()
-# driver.find_element(By.XPATH,"//*[@type='search']").send_keys('g7cr',Keys.ENTER)
-# #driver.get('https://www.google.com/maps/dir/12.9614549,77.7108679/g7cr/@12.977352,77.5817119,12z/data=!3m1!5s0x3bae167afe46e27b:0xf955ec9efa77b1be!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bae167e2fa71621:0x1f56c87698101c58!2m2!1d77.5989907!2d12.9760336?entry=ttu')
-# driver.find_element(By.XPATH,"(//div[@class='QqG1Sd'])[2]").click()
-# time.sleep(3)
-# print(driver.current_url)
-
-
+@then(u'He Make Csv File Of That')
+def makeing_csv_file(context):
+    field_names = ['name', 'address', 'rating', 'reviews', 'Log_and_Lat']
+    with open('company_report.csv', 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerows(Details)
--- Features/companydetails.feature
+++ Features/companydetails.feature
@@ -3,16 +3,17 @@ Feature: Company Details
   Scenario: Fetching Company Details
     Given He Opened Google Page
     When He Search Company Name "Actualize Consulting Engineers"
-    When He Look For Card Apperance
-    Then He Save Details Of Company
+    When He Save Details Of Company
+    Then He Make Csv File Of That
 
   Scenario Outline: Details Of Company
     Given He Opened Google Page
     When He Search Company Name "<company_name>"
-    When He Look For Card Apperance
-    Then He Save Details Of Company
+    When He Save Details Of Company
+    Then He Make Csv File Of That
     Examples:
       | company_name               |
+      | Tech Mahindra              |
       | g7cr                       |
       | Fime India Private Limited |
       |TCS                         |
